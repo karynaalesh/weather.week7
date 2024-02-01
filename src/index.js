f2c30c97ba545877ffdf2690a7e0e9597c234c57
karynaalesh <karyna.aleshchenko@gmail.com>
2024-02-01 16:10:15 +0000

    Added the basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
