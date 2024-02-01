dc2c892cb405ab42aa2a6250b58ecd03bf13b0eb
karynaalesh <karyna.aleshchenko@gmail.com>
2024-02-01 18:34:15 +0000

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+function updateInfo(responce){
+   
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = responce.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = responce.data.city;
+
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+let apiKey = "d08fodtb894540abf5faf86b23ef312e";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+axios.get(apiUrl).then(updateInfo);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+ 
+    searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Manchester");
