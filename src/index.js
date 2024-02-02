6e79efacf57c53076d01adea09a218070d087a70
karynaalesh <karyna.aleshchenko@gmail.com>
2024-02-02 13:06:22 +0000

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -4,9 +4,36 @@ function updateInfo(responce){
     let temperature = responce.data.temperature.current;
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = responce.data.city;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (responce.data.time * 1000);
 
 
+
+
+
+descriptionElement.innerHTML = responce.data.condition.description;
     temperatureElement.innerHTML = Math.round(temperature);
+    humidityElement.innerHTML = `${responce.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${responce.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
